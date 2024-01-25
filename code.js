a2edcbb224b8e98d4bb796f4139ea8085bb0c393
DanPlowman <47125582+DanPlowman@users.noreply.github.com>
2024-01-25 12:17:26 -0700

    Update code.js

--- code.js
+++ code.js
@@ -3,23 +3,15 @@ function divideAndConquerSum(a) {
         {
             return 0;
         }
-        else if(a.length ===1)
+        else if (a.length ===1)
         {
             return a[0];
         }
-        else if(a.length ===2)
-        {
-            return a[0]+a[1];
-        }
-        else if(a.length ===3)
-        {
-            return a[0]+a[1]+a[2];
-        }
-        else
+        else 
         {
         var min =0;
         var max = a.length;
-        var split = Math.floor((min+max/3))
+        var split = Math.ceil((min+max/3))
         let b=a.splice(0,split);
         let c=a.splice(0,split);
 
