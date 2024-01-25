6d38d9b2e9932d903fa16e12c22fae61149bd1ed
DanPlowman <47125582+DanPlowman@users.noreply.github.com>
2024-01-24 17:03:46 -0700

    Update code.js
    this is just a draft, saving it for later

--- code.js
+++ code.js
@@ -1,3 +1,25 @@
 function divideAndConquerSum(a) {
-    return a;
+        if (a.length ===0)
+        {
+            return 0;
+        }
+        else if(a.length ===1)
+        {
+            return a[0];
+        }
+        else if(a.length <3)
+        {
+            return divideAndConquerSum(a[0])+divideAndConquerSum(a[1])+divideAndConquerSum(a[2]);
+        }
+        else
+        {
+        var min =0;
+        var max = a.length;
+        var split = Math.floor((min+max/3))
+        var Lmid = split;
+        var Umid = max-split;
+            return divideAndConquerSum(a.slice(0,(split))) + divideAndConquerSum(a.slice((split),(max-split))) + divideAndConquerSum(a.slice((max-split)));
+        }
 }
+
+console.log(divideAndConquerSum([1,5,2,6,-2,-7,2,7,-5,6,9,-2,2,6]))
